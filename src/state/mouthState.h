87cb2ddaedd9d01df31414ee2f33f4b888080f9e
Kimmix <kimmix05@gmail.com>
2023-03-23 18:41:31 +0700

    mouth viseme

--- src/state/mouthState.h
+++ src/state/mouthState.h
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <arduinoFFT.h>
 #include <driver/i2s.h>
+#include "mouthBitmap.h"
+
 
 #define AH_MIN 600
 #define AH_MAX 1200
@@ -31,7 +33,7 @@ public:
             display->drawMouth(mouthDefault);
             break;
         case BOOP:
-            display->drawMouth(mouthAh);
+            display->drawMouth(mouthOpen);
             currentState = IDLE;
             break;
         case TALKING:
@@ -58,6 +60,7 @@ private:
     double fft_input[SAMPLES],
         fft_output[SAMPLES],
         loudness_thresholds[4] = { 500, 1000, 1500, 2000 };
+    int levelofLoudness = 3;
     arduinoFFT FFT = arduinoFFT(fft_input, fft_output, SAMPLES, SAMPLE_RATE);
 
 
@@ -66,11 +69,42 @@ private:
         BOOP,
         TALKING,
     };
-
     State currentState;
+
+    enum Viseme {
+        AH,
+        EE,
+        OH,
+        OO,
+        TH
+    };
+    // ---- Viseme ----
+    const uint8_t* ahViseme[3] = { AH1, AH2, AH3 };
+    const uint8_t* eeViseme[3] = { EE1, EE2, EE3 };
+    const uint8_t* ohViseme[3] = { OH1, OH2, OH3 };
+    const uint8_t* ooViseme[3] = { OO1, OO2, OO3 };
+    const uint8_t* thViseme[3] = { TH1, TH2, TH3 };
+
+    const uint8_t* visemeOutput(Viseme viseme, int level) {
+        switch (viseme) {
+        case AH:
+            return ahViseme[level];
+        case EE:
+            return eeViseme[level];
+        case OH:
+            return ohViseme[level];
+        case OO:
+            return ooViseme[level];
+        case TH:
+            return thViseme[level];
+        }
+    }
+
     unsigned long microseconds;
     unsigned long sampling_period_us;
     void talking() {
+        double min_amplitude = INFINITY;
+        double max_amplitude = 0;
         // Read microphone input and fill fft_input array with samples
         // int16_t buffer[SAMPLES];
         // mic->read(buffer, SAMPLES);
@@ -112,59 +146,73 @@ private:
             if (freq >= TH_MIN && freq <= TH_MAX) {
                 th_amplitude += amplitude;
             }
-        }
-
-        // Determine which viseme to display based on amplitude and threshold values
-        String viseme = "ah";
-        double viseme_amplitude = ah_amplitude;
-        if (ee_amplitude > viseme_amplitude) {
-            viseme = "ee";
-            viseme_amplitude = ee_amplitude;
-        }
-        if (oh_amplitude > viseme_amplitude) {
-            viseme = "oh";
-            viseme_amplitude = oh_amplitude;
-        }
-        if (oo_amplitude > viseme_amplitude) {
-            viseme = "oo";
-            viseme_amplitude = oo_amplitude;
-        }
-        if (th_amplitude > viseme_amplitude) {
-            viseme = "th";
+            if (amplitude < min_amplitude) {
+                min_amplitude = amplitude;
+            }
+            if (amplitude > max_amplitude) {
+                max_amplitude = amplitude;
+            }
         }
 
         // Compute loudness level based on average amplitude
         double avg_amplitude = abs(ah_amplitude + ee_amplitude + oh_amplitude + oo_amplitude + th_amplitude) / 5.0;
+        double dynamic_threshold = (max_amplitude + min_amplitude) / 2;
         int loudness_level = 0;
-        for (int i = 0; i < 4; i++) {
-            if (avg_amplitude > loudness_thresholds[i]) {
-                loudness_level = i + 1;
-            }
+        if (avg_amplitude > dynamic_threshold * 2) {
+            loudness_level = 2;
         }
-        // Serial.print("AH:");
-        // Serial.print(ah_amplitude);
-        // Serial.print(",");
-        // Serial.print("EE:");
-        // Serial.print(ee_amplitude);
-        // Serial.print(",");
-        // Serial.print("OH:");
-        // Serial.print(oh_amplitude);
-        // Serial.print(",");
-        // Serial.print("OO:");
-        // Serial.print(oo_amplitude);
-        // Serial.print(",");
-        // Serial.print("TH:");
-        // Serial.print(th_amplitude);
-        // Serial.print(",");
-        // Serial.print("AVG_AMP:");
-        // Serial.println(avg_amplitude);
-        // Serial.print(",");
+        else if (avg_amplitude > dynamic_threshold) {
+            loudness_level = 1;
+        }
+
+        // Determine which viseme to display based on amplitude and threshold values
+        Viseme viseme = AH;
+        double viseme_amplitude = ah_amplitude;
+        if (ee_amplitude > viseme_amplitude) {
+            viseme = EE;
+            viseme_amplitude = ee_amplitude;
+        }
+        if (oh_amplitude > viseme_amplitude) {
+            viseme = OH;
+            viseme_amplitude = oh_amplitude;
+        }
+        if (oo_amplitude > viseme_amplitude) {
+            viseme = OO;
+            viseme_amplitude = oo_amplitude;
+        }
+        if (th_amplitude > viseme_amplitude) {
+            viseme = TH;
+        }
+
+        Serial.print("AH:");
+        Serial.print(ah_amplitude);
+        Serial.print(",");
+        Serial.print("EE:");
+        Serial.print(ee_amplitude);
+        Serial.print(",");
+        Serial.print("OH:");
+        Serial.print(oh_amplitude);
+        Serial.print(",");
+        Serial.print("OO:");
+        Serial.print(oo_amplitude);
+        Serial.print(",");
+        Serial.print("TH:");
+        Serial.print(th_amplitude);
+        Serial.print(",");
+        Serial.print("AVG_AMP:");
+        Serial.print(avg_amplitude);
+        Serial.print(",");
+        Serial.print("threshold:");
+        Serial.println(dynamic_threshold);
 
         // Print results
-        Serial.print("Viseme: ");
-        Serial.print(viseme);
-        Serial.print("| Loudness:");
-        Serial.print(loudness_level);
-        Serial.println(" dB");
+        // Serial.print("Viseme: ");
+        // Serial.print(viseme);
+        // Serial.print("| Loudness:");
+        // Serial.print(loudness_level);
+        // Serial.println(" dB");
+
+        // Final render
+        display->drawMouth(visemeOutput(viseme, loudness_level));
     }
 };
