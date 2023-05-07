d1dc8922a702cb664d1fbcd2ad1e8e9b2ab09a1b
Kimmix <kimmix05@gmail.com>
2023-05-07 21:39:17 +0700

    clean up mouth controller

--- src/Controller/mouthController.h
+++ src/Controller/mouthController.h
@@ -1,22 +1,6 @@
 #include "Bitmaps/mouthBitmap.h"
 #include "RenderFunction/viseme.h"
 
-#define AH_MIN 600
-#define AH_MAX 1200
-#define EE_MIN 1000
-#define EE_MAX 2000
-#define OH_MIN 1800
-#define OH_MAX 2800
-#define OO_MIN 2600
-#define OO_MAX 3600
-#define TH_MIN 3400
-#define TH_MAX 4000
-
-#define SAMPLE_RATE 8000
-#define SAMPLES 256
-
-#define NOISE_THRESHOLD 300
-
 class MouthState {
    public:
     MouthState(DisplayController* displayPtr = nullptr) : display(displayPtr),
@@ -34,7 +18,7 @@ class MouthState {
                 currentState = TALKING;
                 break;
             case TALKING:
-                talking();
+                display->drawMouth(viseme.renderViseme());
                 break;
             default:
                 break;
@@ -61,8 +45,4 @@ class MouthState {
         TALKING,
     };
     State currentState;
-
-    void talking() {
-        display->drawMouth(viseme.renderViseme());
-    }
 };
