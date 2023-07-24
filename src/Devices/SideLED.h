3d980b8eb747da11ec71fab112ff9f19a6e0e7d1
Kimmix <kimmix05@gmail.com>
2023-07-24 21:26:45 +0700

    use fastled

--- src/Devices/SideLED.h
+++ src/Devices/SideLED.h
@@ -1,30 +1,63 @@
-#include <sk.h>
+#include <Arduino.h>
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_Sensor.h>
+#include <FastLED.h>
 
-#define SK_PIN 18
-#define LED_COUNT 7
+#define LED_PIN GPIO_NUM_2  // Pin connected to the Data Input of WS2812
+#define NUM_LEDS 7          // Number of WS2812 LEDs in your strip
 
-class SideLED {
-   private:
-    sk led;
+#define BRIGHTNESS 40    // Set the initial brightness (0-255)
+#define LED_TYPE WS2812  // WS2812 or WS2812B, depending on your LEDs
+#define COLOR_ORDER GRB  // GRB or RGB, depending on your LEDs
 
-   public:
-    void init() {
-        led.begin(SK_PIN, LED_COUNT);
-        led.setbrightness(50);
+CRGB leds[NUM_LEDS];
+
+void bounceGradient() {
+    static uint8_t startIndex = 0;  // The starting index of the gradient
+    static bool forward = true;     // Flag to indicate the direction of the gradient
+
+    // Define the new gradient colors in HSV
+    CHSV color1HSV(0, 255, 255);   // Start color: #FF446C (Reddish Pink)
+    CHSV color2HSV(20, 255, 255);  // End color: #F9826C (Coral)
+
+    // Calculate the step for each color component
+    int hueStep = (color2HSV.h - color1HSV.h) / NUM_LEDS;
+    int saturationStep = (color2HSV.s - color1HSV.s) / NUM_LEDS;
+    int valueStep = (color2HSV.v - color1HSV.v) / NUM_LEDS;
+
+    // Fill the LED strip with the gradient
+    for (int i = 0; i < NUM_LEDS; i++) {
+        int index = (startIndex + i) % NUM_LEDS;  // Calculate the current index for the gradient
+        CHSV colorHSV(color1HSV.h + hueStep * i, color1HSV.s + saturationStep * i, color1HSV.v + valueStep * i);
+        CRGB colorRGB;
+        hsv2rgb_rainbow(colorHSV, colorRGB);
+        leds[index] = colorRGB;
     }
 
-    void loop() {
-        // set all LEDs to red
-        // for (int i = 0; i < led.ledcount(); i++) {
-        //     led.color(i, 255, 68, 108, 0);
-        // }
-        led.color(0, 255, 0, 0, 0);
-        led.color(1, 0, 255, 0, 0);
-        led.color(2, 0, 0, 255, 0);
-        led.color(3, 255, 255, 0, 0);
-        led.color(4, 0, 255, 255, 0);
-        led.color(5, 255, 0, 255, 0);
-        led.color(6, 255, 255, 0, 0);
-        led.show();
+    // Move the starting index based on the direction
+    if (forward) {
+        startIndex++;
+        if (startIndex >= NUM_LEDS) {
+            forward = false;  // Change direction when reaching the end
+            startIndex = NUM_LEDS - 1;
+        }
+    } else {
+        startIndex--;
+        if (startIndex <= 0) {
+            forward = true;  // Change direction when reaching the beginning
+            startIndex = 0;
+        }
     }
-};
+}
+
+void setup() {
+    Serial.begin(115200);
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
+    FastLED.setBrightness(BRIGHTNESS);
+}
+
+void loop() {
+    bounceGradient();
+    FastLED.show();
+    delay(100);
+}
