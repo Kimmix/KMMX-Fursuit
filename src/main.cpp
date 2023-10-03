a73c35f6e691f5d538201416a62b95d3b7e9d32e
Kimmix <kimmix05@gmail.com>
2023-10-03 20:32:32 +0700

    add sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <ArduinoBLE.h>
 #include <esp_random.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
 #include "helperFunction.h"
 #include "Controller/controller.h"
@@ -72,10 +74,52 @@ void setupBLE() {
     Serial.println(F("Bluetooth® device active, waiting for connections..."));
 }
 
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+    char a[32];
+    int b;
+    float c;
+    bool d;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t* mac, const uint8_t* incomingData, int len) {
+    memcpy(&myData, incomingData, sizeof(myData));
+    Serial.print("Bytes received: ");
+    Serial.println(len);
+    Serial.print("Char: ");
+    Serial.println(myData.a);
+    Serial.print("Int: ");
+    Serial.println(myData.b);
+    Serial.print("Float: ");
+    Serial.println(myData.c);
+    Serial.print("Bool: ");
+    Serial.println(myData.d);
+    Serial.println();
+}
+
+void setupEspNow() {
+    // Set device as a Wi-Fi Station
+    WiFi.mode(WIFI_STA);
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK) {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+    // Once ESPNow is successfully Init, we will register for recv CB to
+    // get recv packer info
+    esp_now_register_recv_cb(OnDataRecv);
+}
+
 //! -------------------- Main Setup --------------------
 void setup() {
     Serial.begin(115200);
     while (!Serial) delay(400);
+    setupEspNow();
     pinMode(IR_PIN, INPUT);
     randomSeed(analogRead(RANDOM_PIN));
     setupBLE();
