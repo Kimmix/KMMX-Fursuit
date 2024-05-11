dd0955c2beb885eb573a4ab851d47d7b3a06d6dc
Kimmix <kimmix05@gmail.com>
2024-05-11 23:44:43 +0700

    update side led

--- src/Devices/SideLED.h
+++ src/Devices/SideLED.h
@@ -8,46 +8,40 @@ class SideLED {
     SideLED() {
         FastLED.addLeds<LED_TYPE, ARGB, COLOR_ORDER>(leds, ARGB_NUM).setCorrection(TypicalLEDStrip);
         FastLED.setBrightness(brightness);
-        for (short i = 0; i < ARGB_NUM; i++) {
-            leds[i] = CHSV(150, 255, 200);
-        }
     }
 
     void animate() {
+        static uint8_t startIndex = 0;
+        startIndex = startIndex + 1; /* motion speed */
         if (millis() >= prevTime) {
-            prevTime = millis() + 500;
-            updateLEDs();
+            prevTime = millis() + 400;
+            FillLEDsFromPaletteColors(startIndex);
             FastLED.show();
         }
     }
 
    private:
     CRGB leds[ARGB_NUM];
-    const short steps = 24,
-                brightness = 255;
-    short stepsCycle = 0, stepsCycleAlt = steps;
+    const short brightness = 255;
     unsigned long prevTime = 0;
 
-    void updateLEDs() {
-        // Define the new gradient colors in HSV
+    void FillLEDsFromPaletteColors(uint8_t colorIndex) {
+        for (int i = 0; i < ARGB_NUM; i++) {
+            // Get the target color from the palette
+            CRGB targetColor = ColorFromPalette(lavaLampPalette(), colorIndex, brightness, LINEARBLEND);
+            // Interpolate between the current color and the target color
+            leds[i] = blend(leds[i], targetColor, 128);
+            colorIndex += 3;  // Increment the index by the calculated delta
+        }
+    }
+
+    CRGBPalette16 lavaLampPalette() {
         CHSV color1HSV(239, 240, 135);  // Start color: #FF446C (Reddish Pink)
         CHSV color2HSV(22, 185, 125);   // End color: #F9826C (Coral)
-
-        // Calculate the number of steps required for the hsv shift
-        const int hueStep = ((color2HSV.h - color1HSV.h + 256) % 256) / steps;
-        const int saturationStep = (color2HSV.s - color1HSV.s) / steps;
-        const int valueStep = (color2HSV.v - color1HSV.v) / steps;
-
-        stepsCycle = (stepsCycle + 1) % (steps / 2);
-        int currentHue = (color1HSV.h + hueStep * stepsCycle + 256) % 256;  // Ensure the hue wraps around 256
-        int currentSaturation = color1HSV.s + saturationStep * stepsCycle;
-        int currentBrightness = color1HSV.v + valueStep * stepsCycle;
-        leds[esp_random() % ARGB_NUM] = CHSV(currentHue, currentSaturation, currentBrightness);
-        // Alternate
-        stepsCycleAlt = ((stepsCycleAlt + 1) % (steps / 2)) + (steps / 2);
-        int currentHueAlt = (color1HSV.h + hueStep * stepsCycleAlt + 256) % 256;  // Ensure the hue wraps around 256
-        int currentSaturationAlt = color1HSV.s + saturationStep * stepsCycleAlt;
-        int currentBrightnessAlt = color1HSV.v + valueStep * stepsCycleAlt;
-        leds[esp_random() % ARGB_NUM] = CHSV(currentHueAlt, currentSaturationAlt, currentBrightnessAlt);
+        return CRGBPalette16(
+            color1HSV, color2HSV, color1HSV, color2HSV,
+            color1HSV, color2HSV, color1HSV, color2HSV,
+            color1HSV, color2HSV, color1HSV, color2HSV,
+            color1HSV, color2HSV, color1HSV, color2HSV);
     }
 };
