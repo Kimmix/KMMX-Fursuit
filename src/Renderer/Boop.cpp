b284fabfbbce41a190992e1eefda0e6af1ddedef
Kimmix <kimmix05@gmail.com>
2024-08-04 17:28:40 +0700

    optimize

--- src/Renderer/Boop.cpp
+++ src/Renderer/Boop.cpp
@@ -7,51 +7,47 @@ float Boop::calculateBoopSpeed() {
 }
 
 void Boop::getBoop(uint16_t& sensorValue, bool& isInRange, bool& isBoop, float& boopSpeed, bool& isContinuous, bool& isAngry) {
+    isInRange = false;
+    isBoop = false;
+    isContinuous = false;
+    isAngry = false;
+
     switch (currentBoopState) {
         case IDLE:
-            isInRange = false;
-            isBoop = false;
-            isContinuous = false;
-            isAngry = false;
             if (sensorValue > IrMinThreshold && sensorValue < IrMaxThreshold) {
                 currentBoopState = BOOP_IN_PROGRESS;
                 boopStartTime = millis();
+                isInRange = true;
             } else if (sensorValue >= 1023) {
                 currentBoopState = ANGRY;
+                isAngry = true;
             }
             break;
+
         case BOOP_IN_PROGRESS:
             isInRange = true;
-            isBoop = false;
-            isContinuous = false;
-            isAngry = false;
             if (sensorValue >= IrMaxThreshold) {
                 boopSpeed = calculateBoopSpeed();
                 if (boopSpeed > 0.0) {
                     isBoop = true;
-                    if (sensorValue >= IrMaxThreshold) {
-                        currentBoopState = BOOP_CONTINUOUS;
-                        break;
-                    }
+                    currentBoopState = BOOP_CONTINUOUS;
+                    isContinuous = true;
+                } else {
+                    currentBoopState = IDLE;
                 }
-                currentBoopState = IDLE;
             } else if (sensorValue < IrMinThreshold) {
                 currentBoopState = IDLE;
             }
             break;
+
         case BOOP_CONTINUOUS:
-            isInRange = false;
-            isBoop = false;
             isContinuous = true;
-            isAngry = false;
             if (sensorValue < IrMaxThreshold) {
                 currentBoopState = IDLE;
             }
             break;
+
         case ANGRY:
-            isInRange = false;
-            isBoop = false;
-            isContinuous = false;
             isAngry = true;
             if (sensorValue < IrMaxThreshold) {
                 currentBoopState = IDLE;
