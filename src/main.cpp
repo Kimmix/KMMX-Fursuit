f43fcd27a693bc01635df8c500e6e4a5093ff28f
Kimmix <kimmix05@gmail.com>
2025-05-13 09:31:11 +0700

    check psram

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,19 @@ BLEManager& bleManager = BLEManager::getInstance(controller);
 void setup() {
     Wire.begin(S3_SDA, S3_SCL);
     Serial.begin(115200);
+    if (psramInit()) {
+        Serial.println("PSRAM initialized successfully");
+        Serial.printf("Total heap: %d\n", ESP.getHeapSize());
+        Serial.printf("Free heap: %d\n", ESP.getFreeHeap());
+        Serial.printf("Total PSRAM: %d\n", ESP.getPsramSize());
+        Serial.printf("Free PSRAM: %d\n", ESP.getFreePsram());
+    } else {
+        Serial.println("PSRAM initialization failed");
+    }
     randomSeed(analogRead(RANDOM_PIN));
     controller.setupSensors();
     bleManager.setup();
     // setupEspNow();
-    Serial.printf("Free heap: %d, Free PSRAM: %d\n",
-                  ESP.getFreeHeap(),
-                  ESP.getFreePsram());
 }
 
 void loop() {
