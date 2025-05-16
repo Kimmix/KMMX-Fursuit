27dba00065968d05f4039833787d5012b76a8a8f
Kimmix <kimmix05@gmail.com>
2025-05-17 00:54:12 +0700

    swtich bitmap files

--- src/Bitmaps/Bitmaps.h
+++ src/Bitmaps/Bitmaps.h
@@ -4,5 +4,10 @@
 #include "accColor.h"
 #include "Icons.h"
 
+#if defined(USE_RLE_BITMAPS) && USE_RLE_BITMAPS
 #include "mouth_RLE/mouthBitmaps.h"
 #include "eyes_RLE/eyeBitmaps.h"
+#else
+#include "mouth/mouthBitmaps.h"
+#include "eyes/eyeBitmaps.h"
+#endif
